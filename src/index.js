a6a079b867b5e6f9d41f99680c3ab62b14a36141
Valeria Kanonik <needle0606@gmail.com>
2019-10-02 23:03:59 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let leftBrackets = [];
+  let rightBrackets = [];
+  bracketsConfig.forEach(element => {
+      leftBrackets.push(element[0]);
+      rightBrackets.push(element[1]);
+  });
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let comparableChar = str[i];
+    if (leftBrackets.includes(comparableChar) && rightBrackets.includes(comparableChar)) {
+        if (stack.length == 0) {
+            stack.push(comparableChar)
+            continue
+        }
+        if (stack[stack.length - 1] == comparableChar) {
+            stack.pop()
+        } else {
+            stack.push(comparableChar)
+        }
+        continue
+    }
+
+    if (leftBrackets.includes(comparableChar)) {
+        stack.push(comparableChar);
+        continue
+    }
+
+    if (rightBrackets.includes(comparableChar)) {
+        if (stack.length == 0) return false;
+        let poppedBracket = stack.pop();
+        if (leftBrackets.indexOf(poppedBracket) != rightBrackets.indexOf(comparableChar)) return false
+    }
+  }
+    if (stack.length > 0) return false
+
+  return true;
 }
